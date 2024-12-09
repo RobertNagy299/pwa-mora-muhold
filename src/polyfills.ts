7a6572aef8ad5e9788e2d6fe653d2a148d91f472
RobertNagy299 <nagyrobertoffical@gmail.com>
2024-12-10 00:59:45 +0100

    SERVICE WORKER WORKS

---
+++ src/polyfills.ts
@@ -0,0 +1,6 @@
+// Import the necessary polyfills.
+import 'zone.js';  // Included with Angular.
+
+// Add any other necessary polyfills for your app.
+// Example for modern browsers:
+// import 'core-js/es/reflect';
