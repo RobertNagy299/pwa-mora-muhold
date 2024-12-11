daca435df30a54791c427056e30f22350d548c12
RobertNagy299 <nagyrobertoffical@gmail.com>
2024-12-11 20:45:38 +0100

    final version upload before removing node modules

--- src/app/services/connectivity.service.ts
+++ src/app/services/connectivity.service.ts
@@ -1,40 +1,70 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, interval, Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
+// import { Injectable } from '@angular/core';
+// import { BehaviorSubject, interval, Observable } from 'rxjs';
+// import { switchMap } from 'rxjs/operators';
+// import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
+//
+// @UntilDestroy()
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class ConnectivityService {
+//   private onlineStatusSubject = new BehaviorSubject<boolean>(navigator.onLine);
+//   public onlineStatus$: Observable<boolean> = this.onlineStatusSubject.asObservable();
+//
+//   constructor() {
+//     this.checkOnlineStatus();
+//   }
+//
+//   private checkOnlineStatus(): void {
+//     interval(5000).pipe( // Check every 5 seconds
+//       switchMap(() => this.fetchResource())
+//     ).pipe(untilDestroyed(this)).subscribe(
+//       (isOnline) => this.onlineStatusSubject.next(isOnline),
+//       (error) => this.onlineStatusSubject.next(false)
+//     );
+//   }
+//
+//   private fetchResource(): Observable<boolean> {
+//     return new Observable((observer) => {
+//       fetch('https://www.google.com', { method: 'HEAD', mode: 'no-cors' })
+//         .then(() => {
+//           observer.next(true);
+//           observer.complete();
+//         })
+//         .catch(() => {
+//           observer.next(false);
+//           observer.complete();
+//         });
+//     });
+//   }
+// }
+import {Injectable, OnDestroy} from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
-@UntilDestroy()
 @Injectable({
   providedIn: 'root',
 })
-export class ConnectivityService {
+export class ConnectivityService implements OnDestroy {
   private onlineStatusSubject = new BehaviorSubject<boolean>(navigator.onLine);
   public onlineStatus$: Observable<boolean> = this.onlineStatusSubject.asObservable();
 
+  private onlineListener = () => this.updateOnlineStatus(true);
+  private offlineListener = () => this.updateOnlineStatus(false);
   constructor() {
-    this.checkOnlineStatus();
+    this.initializeOnlineStatusListener();
   }
 
-  private checkOnlineStatus(): void {
-    interval(5000).pipe( // Check every 5 seconds
-      switchMap(() => this.fetchResource())
-    ).pipe(untilDestroyed(this)).subscribe(
-      (isOnline) => this.onlineStatusSubject.next(isOnline),
-      (error) => this.onlineStatusSubject.next(false)
-    );
+  private initializeOnlineStatusListener(): void {
+    window.addEventListener('online', this.onlineListener);
+    window.addEventListener('offline', this.offlineListener);
   }
 
-  private fetchResource(): Observable<boolean> {
-    return new Observable((observer) => {
-      fetch('https://www.google.com', { method: 'HEAD', mode: 'no-cors' })
-        .then(() => {
-          observer.next(true);
-          observer.complete();
-        })
-        .catch(() => {
-          observer.next(false);
-          observer.complete();
-        });
-    });
+  ngOnDestroy() {
+    window.removeEventListener('online', this.onlineListener);
+    window.removeEventListener('offline', this.offlineListener);
+  }
+
+  private updateOnlineStatus(isOnline: boolean): void {
+    this.onlineStatusSubject.next(isOnline);
   }
 }
